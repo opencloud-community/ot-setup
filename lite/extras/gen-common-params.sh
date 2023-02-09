eed7bcaf8d6a8075c8234b9333f1e5a2005d5fdc
André Fischer <a.fischer@opentalk.eu>
2023-02-09 09:52:34 +0100

    change defaults and sample config

--- lite/extras/gen-common-params.sh
+++ lite/extras/gen-common-params.sh
@@ -4,14 +4,14 @@
 
 if type "pwgen" > /dev/null; then
   echo "###---> Common variables"
-  # prit hostname
+  # print hostname
   echo "# Domain name on wich you want to access the frontend"
   echo "OT_DOMAIN=$(hostnamectl hostname)"
   # gen secrets for postgresm keycloak admin and keycloak client
   echo -e "\nPOSTGRES_PASSWORD=$(pwgen 24)\nKEYCLOAK_ADMIN_PASSWORD=$(pwgen 24)\nKC_CLIENT_SECRET=$(pwgen 24) \n"
   # print ip adresses to use for rabbitmq connection
   echo "# If janus is running in docker host mode it needs a local host interface for rabbitmq to connect."
-  echo "# Use only a SINGE line/interface and uncomment it."
+  echo "# Use only a SINGLE line/interface and uncomment it."
   echo "# !!! DO NOT CHOOSE YOUR PUBLIC IP ADDRESS!!!"
   for IP in $(ip -o -4 addr show | awk '{ split($4, ip_addr, "/"); print ip_addr[1] }'| grep -v '127.0.0.1'); do
      echo "# RABBITMQ_HOST=${IP}"
