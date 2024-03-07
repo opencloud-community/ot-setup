d739e20f58a34ef46f0643dd3020a54d305a496b
André Fischer <a.fischer@opentalk.eu>
2024-03-07 10:39:41 +0100

    improve usage of pwgen

--- extras/gen-secrets.sh
+++ extras/gen-secrets.sh
@@ -2,14 +2,14 @@
 
 # a simple shell script, to quickly generate some secrets to place in .env file
 if type "pwgen" > /dev/null; then
-  echo "POSTGRES_PASSWORD=$(pwgen 32)"
-  echo "KEYCLOAK_ADMIN_PASSWORD=$(pwgen 32)"
-  echo "KEYCLOAK_CLIENT_SECRET_CONTROLLER=$(pwgen 32)"
-  echo "KEYCLOAK_CLIENT_SECRET_OBELISK=$(pwgen 32)"
-  echo "KEYCLOAK_CLIENT_SECRET_RECORDER=$(pwgen 32)"
-  echo "SPACEDECK_API_TOKEN=$(pwgen 32)"
-  echo "SPACEDECK_INVITE_CODE=$(pwgen 32)"
-  echo "ETHERPAD_API_KEY=$(pwgen 32)"
+  echo "POSTGRES_PASSWORD=$(pwgen -s 32 1)"
+  echo "KEYCLOAK_ADMIN_PASSWORD=$(pwgen -s 32 1)"
+  echo "KEYCLOAK_CLIENT_SECRET_CONTROLLER=$(pwgen -s 32 1)"
+  echo "KEYCLOAK_CLIENT_SECRET_OBELISK=$(pwgen -s 32 1)"
+  echo "KEYCLOAK_CLIENT_SECRET_RECORDER=$(pwgen -s 32 1)"
+  echo "SPACEDECK_API_TOKEN=$(pwgen -s 32 1)"
+  echo "SPACEDECK_INVITE_CODE=$(pwgen -s 32 1)"
+  echo "ETHERPAD_API_KEY=$(pwgen -s 32 1)"
 else
   echo "the utility 'pwgen' needs to be installed."
   exit 1  
