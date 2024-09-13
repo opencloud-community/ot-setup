c476d3bc0784c343f998fd0a1d377ac42ef27b27
André Fischer <a.fischer@opentalk.eu>
2024-09-13 17:22:14 +0200

    remove comment

--- init/livekit-init.sh
+++ init/livekit-init.sh
@@ -6,7 +6,6 @@ port: $LIVEKIT_HTTP_PORT
 rtc:
   use_external_ip: $LIVEKIT_RTC_USE_EXTERNAL_IP
   tcp_port: $LIVEKIT_TCP_PORT
-  # udp_port: 7882
   port_range_start: $LIVEKIT_RTC_PORT_RANGE_START
   port_range_end: $LIVEKIT_RTC_PORT_RANGE_END
   enable_loopback_candidate: $LIVEKIT_RTC_ENABLE_LOOPBACK_CANDIDATE
