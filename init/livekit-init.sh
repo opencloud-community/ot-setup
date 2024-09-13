21758950281459c46094f320bf5804f173e13f67
André Fischer <a.fischer@opentalk.eu>
2024-09-13 12:31:32 +0200

    add livekit-init.sh & adapt compose file

---
+++ init/livekit-init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cat << EOF > /livekit.yaml
+---
+port: $LIVEKIT_HTTP_PORT
+rtc:
+  use_external_ip: $LIVEKIT_RTC_USE_EXTERNAL_IP
+  tcp_port: $LIVEKIT_TCP_PORT
+  # udp_port: 7882
+  port_range_start: $LIVEKIT_RTC_PORT_RANGE_START
+  port_range_end: $LIVEKIT_RTC_PORT_RANGE_END
+  enable_loopback_candidate: $LIVEKIT_RTC_ENABLE_LOOPBACK_CANDIDATE
+turn: 
+  enabled: $LIVEKIT_TURN_ENABLED
+keys:
+  devkey: $LIVEKIT_KEYS_DEVKEY
+logging:
+  json: $LIVEKIT_LOGGING_JSON
+  level: $LIVEKIT_LOGGING_LEVEL
+EOF
+
+/livekit-server --config /livekit.yaml
+
